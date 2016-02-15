35116814a8eba08e7b53da012592e55af72c40aa
Чингиз Ауанасов <auanassov.chingiz@gmail.com>
2016-02-15 15:28:28 +0500

    - Rails project - Bowerfile assets

---
+++ Gemfile
@@ -0,0 +1,64 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.3'
+gem 'sqlite3'
+gem 'cancancan'
+gem 'hirb-unicode'
+gem 'sass-rails'
+gem "less-rails"
+gem 'coffee-rails', '~> 4.0.0'
+gem 'spring',        group: :development
+gem 'uglifier', '>= 1.3.0'
+gem 'rolify'
+gem 'barby'
+gem 'chunky_png'
+gem 'rack-cors', :require => 'rack/cors'
+gem 'rails_best_practices'
+gem 'digest'
+gem 'thin'
+gem 'active_model_serializers', '~> 0.10.0.rc4'
+gem 'json'
+gem 'logger'
+gem 'bower-rails'
+gem 'haml-rails'
+
+gem 'haml'
+gem 'exception_notification'
+gem 'erb2haml'
+gem 'devise_lastseenable'
+gem 'faker'
+gem 'mongoid', '~> 5.0.0'
+gem 'simple_enum', '~> 2.1.1' , require: 'simple_enum/mongoid'
+gem 'therubyracer'
+gem 'validates_timeliness' , '3.0.14'# Нужна была валидация datetime при добавлении акций
+gem 'devise'
+
+# Sidekiq
+gem 'sinatra'
+gem 'sidekiq'
+gem 'clockwork'
+gem 'httpclient'
+
+gem 'redis'
+gem 'figaro'
+
+group :development, :test do
+  gem 'guard'
+  gem 'spring-commands-rspec'
+  gem 'rspec-rails', '~> 3.1'
+  gem 'rb-fsevent' # if `uname` =~ /Darwin/
+  gem 'guard-rspec', require: false
+  gem 'shoulda'
+  gem 'shoulda-matchers'
+  gem 'quiet_assets'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'email_spec'
+  gem 'timecop'
+  gem 'launchy'
+end
+
---
+++ config/application.rb
@@ -0,0 +1,44 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Practice
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |generate|
+      generate.template_engine :haml
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.views false
+    end
+
+    config.action_view.logger = nil
+    config.time_zone = 'Europe/Moscow'
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
+    config.i18n.locale = :en
+    config.i18n.enforce_available_locales = true
+    I18n.config.enforce_available_locales = true
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.templates_path = Rails.root.join('app', 'views', 'templates')
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap", "assets", "fonts")
+
+    config.generators.assets = false
+    config.generators.helper = false
+    config.generators.views = false
+    config.generators.controller_specs = false
+    config.generators.views_specs = false
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
