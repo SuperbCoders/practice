612a2b663b6b98909c606d22110155a6e86e54f9
Чингиз Ауанасов <auanassov.chingiz@gmail.com>
2016-02-24 17:23:07 +0500

    clear

---
+++ config/unicorn/staging.rb
@@ -0,0 +1,39 @@
+deploy_to  = '/www/dev-pract.onomnenado.ru'
+rails_root = "#{deploy_to}/current"
+pid_file   = "#{deploy_to}/shared/tmp/pids/unicorn.pid"
+log_file   = "#{rails_root}/log/unicorn.log"
+err_log    = "#{rails_root}/log/unicorn_error.log"
+old_pid    = pid_file + '.oldbin'
+
+
+worker_processes 1
+timeout 30
+preload_app true
+listen 8091, tcp_nopush: true # порт может быть другой
+
+pid pid_file
+stderr_path err_log
+stdout_path log_file
+
+GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+end
