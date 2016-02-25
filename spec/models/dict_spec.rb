644621e981727b05dabfe97366dbf1ff5db0e542
Чингиз Ауанасов <auanassov.chingiz@gmail.com>
2016-02-25 11:44:31 +0500

    Duration: 10 minutes. Dict/WorkSchedule rspec

--- spec/models/dict_spec.rb
+++ spec/models/dict_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Dict, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:dict_type, :dict_value) }
+  it { is_expected.to belong_to(:dictable) }
 end
--- spec/models/work_schedule_spec.rb
+++ spec/models/work_schedule_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WorkSchedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:day, :start_at, :finish_at) }
+  it { is_expected.to belong_to(:doctor) }
+  it { is_expected.to validate_presence_of(:start_at) }
+  it { is_expected.to validate_presence_of(:finish_at) }
 end
