05f2ef93572b05ca5708e26b0b00ccfdbfa22ea5
Чингиз Ауанасов <auanassov.chingiz@gmail.com>
2016-02-26 15:20:37 +0500

    fix deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,4 @@ role :db,  %w{w3dev-pract@ono.rrv.ru:2223}
 
 set :application, 'dev-practice'
 set :deploy_to, '/www/dev-pract.onomnenado.ru'
-set :branch, 'doctor_public_profile'
+set :branch, 'staging'
