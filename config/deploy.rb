be86a8faf5a9fe2b74b8f6c1cd00c481dedf8adb
Чингиз Ауанасов <auanassov.chingiz@gmail.com>
2016-02-29 17:03:35 +0500

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,14 @@ set :repo_url, 'git@github.com:SuperbCoders/practice.git'
 set :log_level, :debug
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
+set :rails_env, fetch(:stage)
 
 namespace :deploy do
 
   before :starting, :bower do
     on roles(:app), in: :sequence, wait: 5 do
       within release_path do
-        execute :rake, 'bower:install'
+        # execute :rake, 'bower:install'
       end
     end
   end
@@ -48,5 +49,5 @@ namespace :bower do
   end
 end
 
-after 'deploy:updated', 'bower:install'
+# after 'deploy:updated', 'bower:install'
 before 'deploy:compile_assets', 'bower:install'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,15 +1,7 @@
-
-set :stage, :staging
-
 role :app, %w{w3dev-pract@ono.rrv.ru:2223}
 role :web, %w{w3dev-pract@ono.rrv.ru:2223}
 role :db,  %w{w3dev-pract@ono.rrv.ru:2223}
 
 set :application, 'dev-practice'
 set :deploy_to, '/www/dev-pract.onomnenado.ru'
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-set :format, :pretty
-set :log_level, :debug
-set :pty, true
-# set :branch, '05f2ef93572b05ca5708e26b0b00ccfdbfa22ea5'
+set :branch, 'staging'
