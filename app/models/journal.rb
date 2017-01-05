3b1e5dc1b29e753c3c965990d18cef06276793d1
Evgeny Sherkunov <esherkunov@gmail.com>
2017-01-05 14:42:28 +0300

    Journal records order in patient's journal preview.

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -3,7 +3,7 @@ class Journal < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
 
-  has_many :journal_records, dependent: :destroy
+  has_many :journal_records, -> { order :created_at}, dependent: :destroy
   has_many :attachments, as: :attachable, dependent: :destroy
 
   accepts_nested_attributes_for :journal_records, :attachments
