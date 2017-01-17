20ad708397d56862a6e98e70aeede25820081097
Evgeny Sherkunov <esherkunov@gmail.com>
2017-01-17 15:28:54 +0300

    Sign in and sing up with pop up dialog.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,16 @@ class SessionsController < Devise::SessionsController
     super
   end
 
+  # POST /resource/sign_in
+  def create
+    unless Doctor.exists?(email: resource_params[:email])
+      Doctor.create(
+        resource_params
+          .permit(:email, :password)
+          .merge({ password_confirmation: resource_params.fetch(:password) })
+      )
+    end
+    super
+  end
+
 end
