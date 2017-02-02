d4c9c369bca89aa104fed74c3efb2aaf60e3efef
Evgeny Sherkunov <esherkunov@gmail.com>
2017-02-02 16:02:56 +0300

    Make public access for value lists controller

--- app/controllers/admin/value_lists_controller.rb
+++ app/controllers/admin/value_lists_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ValueListsController < Admin::BaseController
   include Concerns::Resource
 
+  skip_before_action :authenticate_admin!
+
   def show
   	@value_list = ValueList.find_by(name: params[:name])
   	send_json serialize_resource(@value_list, resource_serializer), true
