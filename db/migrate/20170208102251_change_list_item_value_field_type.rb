abeb01bbcf97a3b98810abd78e666decf032cb39
Evgeny Sherkunov <esherkunov@gmail.com>
2017-02-08 13:35:25 +0300

    Revert "Change list item value type to `text`."
    This reverts commit 57cf585be4fafb5b40fad38daa3f79118a9821a0.

--- db/migrate/20170208102251_change_list_item_value_field_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeListItemValueFieldType < ActiveRecord::Migration
-  def self.down
-    change_column :value_list_items, :value, :string
-  end
-
-  def self.up
-    change_column :value_list_items, :value, :text
-  end
-end
