68ebb28779bbfb2daa29675da06f6e41e2b5d5e6
Evgeny Sherkunov <esherkunov@gmail.com>
2017-02-08 17:00:04 +0300

    Add system settings back-end resource.

---
+++ app/controllers/admin/system_settings_controller.rb
@@ -0,0 +1,25 @@
+class Admin::SystemSettingsController < Admin::BaseController
+  include Concerns::Resource
+
+  def index
+    settings = search_proxy.each_with_object({success: true}) { |s, o| o[s[:slug]] = s[:value] }
+    send_response settings
+  end
+
+  def update
+    settings = resource_params.map { |k, v| {slug: k, value: v}}
+    settings.each do |s|
+      SystemSettings.where(slug: s[:slug]).update_all(value: s[:value])
+    end
+    head 200
+  end
+
+  def resource_scope
+    SystemSettings
+  end
+
+  def permitted_params
+    [ :_id,:id, :counter_code]
+  end
+
+end
