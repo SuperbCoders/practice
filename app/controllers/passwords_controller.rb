ada392fb6461684d06fda80dab4617142819b554
Evgeny Sherkunov <esherkunov@gmail.com>
2017-02-21 13:43:11 +0300

    Point devise mailer password recovery action to our email template

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,9 +1,18 @@
 class PasswordsController < Devise::PasswordsController
+
+  layout 'admins_login'
+
   respond_to :json, only: [:create]
 
   # POST /resource/password
   def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    self.resource = resource_class.find_or_initialize_with_errors(resource_class.reset_password_keys, resource_params, :not_found)
+
+    if resource.persisted?
+      token = resource.send :set_reset_password_token
+      DoctorMailer.password_recovery_email(resource, token).deliver_now
+    end
+
     yield resource if block_given?
 
     if successfully_sent?(resource)
