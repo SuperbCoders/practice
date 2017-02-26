20166d953d6462b82a4f6e8ab8cfbfc7e82250c5
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2017-02-26 12:01:26 +0400

    auto redirect logged in user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ActionController::Base
   include Concerns::CounterCode
 
   def landing
-
+    if doctor_signed_in?
+      redirect_to doctor_root_path
+    end
   end
 end
