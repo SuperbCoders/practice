8669787c0311832e70484a5a4f3ec824680fcc0c
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2017-03-01 18:13:38 +0400

    0.47 static pages menu

--- app/assets/javascripts/static/landing/land_script.js
+++ app/assets/javascripts/static/landing/land_script.js
@@ -12,6 +12,16 @@ $(function ($) {
         header.toggleClass('_white', doc.scrollTop() > header.outerHeight());
     });
 
+    $('.scrollTo').on('click', function () {
+        var firedEl = $(this);
+
+        console.log($((firedEl).attr('href')).offset().top);
+        
+        docScrollTo($((firedEl).attr('href')).offset().top - header.outerHeight(), 800);
+
+        return false;
+    });
+
     //----------
 
     var inputEvents = 'keyup,keypress,focus,blur,change'.split(',');
@@ -149,3 +159,11 @@ $(function ($) {
 
 });
 
+function docScrollTo(pos, speed, callback) {
+
+    $('html,body').animate({'scrollTop': pos}, speed, function () {
+        if (typeof(callback) == 'function') {
+            callback();
+        }
+    });
+}
