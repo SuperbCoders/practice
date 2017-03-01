e0191cab2e926abd31fda9f0520e0eede7a0db83
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2017-03-01 20:11:26 +0400

    fix scroll to prices from other pages

--- app/assets/javascripts/static/landing/land_script.js
+++ app/assets/javascripts/static/landing/land_script.js
@@ -22,6 +22,28 @@ $(function ($) {
         return false;
     });
 
+    function findGetParameter(parameterName) {
+        var result = null,
+            tmp = [];
+        location.search
+            .substr(1)
+            .split("&")
+            .forEach(function (item) {
+                tmp = item.split("=");
+                if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+            });
+        return result;
+    }
+
+    function doScrollToOnStart() {
+        var scroll_to;
+        if (scroll_to = findGetParameter('scroll_to')) {
+            docScrollTo($('#' + scroll_to).offset().top - header.outerHeight(), 800);
+        }
+    }
+
+    doScrollToOnStart();
+
     //----------
 
     var inputEvents = 'keyup,keypress,focus,blur,change'.split(',');
