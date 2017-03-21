2237ac644444eb34b54729f485a06fd7d6ee6653
Evgeniy Belyaev <eb.gh.cr@gmail.com>
2017-03-21 22:38:16 +0400

    public form fix wrong field name fix default name email

--- app/controllers/public/profiles_controller.rb
+++ app/controllers/public/profiles_controller.rb
@@ -84,7 +84,7 @@ class Public::ProfilesController < ApplicationController
     else
       @patient = Patient.new(email: Patient.temporary_email, password: Patient.temporary_password)
     end
-    @patient.full_name = patient_params.fetch(:name, Patient.temporary_email)
+    @patient.full_name = patient_params.fetch(:name, @patient.email)
     if @patient.save
       if patient_params[:phone]
         @patient.contacts.phone.find_or_create_by(data: patient_params[:phone])
